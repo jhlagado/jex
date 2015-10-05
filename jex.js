0e9e8b2aead708f6f3288588cb4dfc30d12ee68d
John Hardy <jh@lagado.com>
2015-10-05 23:51:56 +1100

    refined api

--- jex.js
+++ jex.js
@@ -8,56 +8,84 @@
         factory(lib);
         if (typeof define === 'function' && define.amd)
             define(lib); // AMD
-        else
+        else if (!root[lib.name])
             root[lib.name] = lib; // <script>
+        else
+            throw lib.name + " already defined"
     }
 
 })(this, function(lib) {
     
     var services = {};
-
+    
     lib.name = 'jex';
     lib.version = '0.0.0';
-
-    lib.register = register;
+    
+    lib.service = service;
     lib.get = get;
     lib.use = use;
     lib.inject = inject;
 
-    function register(name, deps, func) {
-        if (name in services) throw 'Duplicate service: ' + name;
+    //registers a factory function with a name and also it's named dependencies
+    //these dependencies will be injected into the function as args
+    //the order of these args is the same as the order of the deps
+    //name - string
+    //deps - array of string
+    //factory - function
+    function service(name, deps, factory) {
+        if (name in services)
+            throw 'Duplicate service: ' + name;
         services[name] = {
             deps: deps,
-            func: func,
+            factory: factory,
         }
     }
 
+    //applies and caches a named factory function with all its dependencies applied
+    //name - string
+
+    //returns a cached value for that factory
     function get(name) {
-        if (!(name in services)) throw 'Cannot find service: ' + name;
+        if (!(name in services))
+            throw 'Cannot find service: ' + name;
         var service = services[name];
         if (!('value' in service)) {
-            if (service.resolving) throw 'Circular dependency on self: ' + name;
-            service.resolving = true;
-            service.value = use(service.deps, service.func);
-            service.resolving = false;
+            service.value = {}; 
+//             if (service.resolving)
+//                 throw 'Circular dependency on self: ' + name;
+            service.value.__resolving = true;
+            var value = use(service.deps, service.factory);
+            Object.keys(value).reduce(function(acc,key){
+                acc[key] = value[key];
+                return acc;
+            },service.value);
+         delete service.value.__resolving
         }
         return service.value;
     }
 
-    function use(deps, func){
+    //applies a factory function and injects named dependencies as args
+    //deps - array of string
+    //factory - function
+    function use(deps, factory) {
         var values = deps.map(get);
-        return inject(values, func)    
+        return inject(values, factory)
     }
 
-    //inject values into a function
+    //inject values into a factory function
     //values can be an array or an object
     //if values is an object then the values to 
     //inject are mapped using keys 
-    function inject(values, func, keys) {
+    //values - array of values to inject or an dict with values in its properties
+    //factory - function to be applid
+    //keys - array of keys. Only needed if values is a dict
+    function inject(values, factory, keys) {
         if (keys) {
-            values = keys.map(function(key){return values[key]});
+            values = keys.map(function(key) {
+                return values[key]
+            });
         }
-        return func.apply(null, values);
+        return factory.apply(null, values);
     }
 
 });
